cefac5ecf9ba63d335c0938fa7b9813e98f660a0
John Doe <johndoe@example.com>
2025-04-29 20:50:18 +0300

    add-settings

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter, status
-from applications.users.schemas import BaseFields, RegisterUserFields
+from applications.users.schemas import RegisterUserFields, BaseFields
+from settings import settings
 
 router_users = APIRouter()
 
-@router_users.post('/create', status_code=status.HTTP_201_CREATED)
-async def create_user(new_user: RegisterUserFields) -> BaseFields:
+
+@router_users.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_user(
+    new_user: RegisterUserFields
+) -> BaseFields:
+    print(settings.POSTGRES_DB, 6666666666666666666666)
     return new_user
