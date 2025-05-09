47c3e193f70842d312f1110a0aebcb1abc73bccc
John Doe <johndoe@example.com>
2025-05-09 20:27:17 +0300

    ALembic

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from applications.users.models import User
 from applications.users.schemas import BaseFields, RegisterUserFields
 from database.session_dependancies import get_async_session
 from settings import settings
@@ -8,7 +8,14 @@ from settings import settings
 router_users = APIRouter()
 
 
+async def create_user(email, name, password, session: AsyncSession):
+    new_user = User(email=email, name=name, hashed_password=password)
+    session.add(new_user)
+    await session.commit()
+
+
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_user(new_user: RegisterUserFields, session: AsyncSession = Depends(get_async_session)) -> BaseFields:
     print(session)
+
     return new_user
