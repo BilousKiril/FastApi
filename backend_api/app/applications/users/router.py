8e12f8545b84566ff08f2ca15549018bd7d393cb
John Doe <johndoe@example.com>
2025-05-09 19:22:05 +0300

    refactor

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, status
-
+from sqlalchemy.ext.asyncio import AsyncSession
 from applications.users.schemas import BaseFields, RegisterUserFields
 from settings import settings
 
@@ -7,6 +7,6 @@ router_users = APIRouter()
 
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(new_user: RegisterUserFields) -> BaseFields:
+async def create_user(new_user: RegisterUserFields, session: AsyncSession) -> BaseFields:
     print(settings.POSTGRES_DB, 6666666666666666666666)
     return new_user
