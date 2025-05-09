92feb421b192f91d21bf908a8d45f9cdf493fcba
John Doe <johndoe@example.com>
2025-05-09 20:08:41 +0300

    Test session

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from applications.users.schemas import BaseFields, RegisterUserFields
+from database.session_dependancies import get_async_session
 from settings import settings
 
 router_users = APIRouter()
 
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(new_user: RegisterUserFields, session: AsyncSession) -> BaseFields:
-    print(settings.POSTGRES_DB, 6666666666666666666666)
+async def create_user(new_user: RegisterUserFields, session: AsyncSession = Depends(get_async_session)) -> BaseFields:
+    print(session)
     return new_user
