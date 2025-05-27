88e3599e036c58248dc9e2c90dd64ab8d51e7948
John Doe <johndoe@example.com>
2025-05-27 21:24:32 +0300

    log

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -1,20 +1,38 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
 from fastapi.templating import Jinja2Templates
+import httpx
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory='templates')
 
+
 @router.get('/')
 async def index(request: Request):
-    context = {'request': request, 'data': 123}
+    context = {'request': request}
     response = templates.TemplateResponse('index.html', context=context)
     return response
+
+async def login_user(user_email: str, password: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            url='http://backend_api_course:9999/api/auth/login',
+            data={"username": user_email, 'password': password}
+
+        )
+        print(response.json())
 
 
 
 @router.get('/login')
-async def login(request: Request):
-    context = {'request': request, 'data': 123}
+@router.post('/login')
+async def login(request: Request, user_email: str = Form(''), password: str = Form('')):
+    print(request.method, 555555555)
+    print(F"{user_email}")
+    print(F"{password}")
+
+    await login_user(user_email, password)
+
+    context = {'request': request}
     response = templates.TemplateResponse('login.html', context=context)
     return response
