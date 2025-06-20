edd1b30c0b9598772f57d43aab16b9e02811d8bf
John Doe <johndoe@example.com>
2025-06-20 20:55:58 +0300

    add search

--- backend_api/app/applications/products/router.py
+++ backend_api/app/applications/products/router.py
@@ -1,19 +1,19 @@
 from fastapi import APIRouter, Body, UploadFile, Depends
 import uuid
-
+from applications.auth.security import admin_required, get_current_user
 from applications.products.crud import create_product_in_db
 from services.s3.s3 import s3_storage
 from sqlalchemy.ext.asyncio import AsyncSession
-from applications.products.schemas import ProductSchema
-
+from applications.products.schemas import ProductSchema, SearchParamsSchema
+from applications.users.models import User
 from applications.users.crud import create_user_in_db, get_user_by_email, activate_user_account
 from applications.users.schemas import BaseUserInfo, RegisterUserFields
 from database.session_dependancies import get_async_session
-
+from typing import Annotated
 products_router = APIRouter()
 
 
-@products_router.post('/')
+@products_router.post('/', dependencies=[Depends(admin_required)])
 async def create_product(
         main_image: UploadFile,
         images: list[UploadFile] = None,
@@ -21,7 +21,7 @@ async def create_product(
         description: str = Body(max_length=1000),
         price: float = Body(gt=1),
         session: AsyncSession = Depends(get_async_session),
-):
+) -> ProductSchema:
     product_uuid = uuid.uuid4()
     main_image = await s3_storage.upload_product_image(main_image, product_uuid=product_uuid)
     images = images or []
@@ -30,9 +30,9 @@ async def create_product(
         url = await s3_storage.upload_product_image(image, product_uuid=product_uuid)
         images_urls.append(url)
 
-    await  create_product_in_db(product_uuid=product_uuid, title=title, description=description, price=price,
+    created_product = await  create_product_in_db(product_uuid=product_uuid, title=title, description=description, price=price,
                                 main_image=main_image, images=images_urls, session=session)
-    return
+    return created_product
 
 
 @products_router.get('/{pk}')
@@ -40,6 +40,5 @@ async def get_product(pk: int):
     return
 
 
-@products_router.get('/')
-async def get_products():
+async def get_products(params: Annotated[SearchParamsSchema, Depends()], session: AsyncSession = Depends(get_async_session)):
     return
